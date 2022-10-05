49c9ef09568b6ec1e555efd76e0834d75d1c8106
IamAbhi7666 <abhishek@atishae.net>
2022-10-05 10:40:33 +0530

    second commit

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -9,30 +9,43 @@ export default component$(() => {
         <div class="container mx-auto fixed top-0 left-0 right-0">
           <div class="text-white ">
             <div class="row">
-              <div class="col-12">
+              <div class="col-12 ">
+                <img src="src/components/images/logo.png" alt="Logo" class="center"/>
                 <ul class="flex flex-row my-8">
-                  <li class="pr-4 hover:text-blue-500 float-left">
-                    <a href="/" target="_self"> 
+                  <li class="pr-4 hover:text-blue-500">
+                    <a href="/" target="_self" class="font-sans text-lg"> 
                       About
                     </a>
                   </li>
 
                     <li class="px-4 hover:text-blue-500">
-                      <a href="/" target="_self">
+                      <a href="/" target="_self" class="font-sans text-lg">
                         Jobs
                       </a>
                     </li>
 
                   <li class="px-4 hover:text-blue-500">
-                    <a href="/" target="_self">
+                    <a href="/" target="_self" class="font-sans text-lg">
                       Blog
                     </a>
                   </li>
 
                   <li class="px-4 hover:text-blue-500">
-                    <a href="/" target="_self">
+                    <a href="/" target="_self" class="font-sans text-lg">
                       Contact
                     </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <ul class="flex flex-row my-8 work">
+                  <li class="pr-4 hover:text-blue-500">
+                    <a href="serviceNav.tsx" class="text-2xl font-sans">Services</a>
+                  </li>
+                  <li class="px-4 hover:text-blue-500">
+                    <a href="/" class="text-2xl font-sans">Out Work</a>
                   </li>
                 </ul>
               </div>
